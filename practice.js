0fdfa9d68e888432232b6cd6563d60310113951e
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-12-11 16:58:36 +0200

    removing the last element from thesring

--- practice.js
+++ practice.js
@@ -153,4 +153,17 @@ Expected
 myFunction('gedcba')
 Expected
 'ged'*/
-// 10.
+// 10.Write a function that takes a string (a) as argument. Remove the last 3 characters of a. Return the result
+
+function myFunction(a) {
+  return a.slice(0, -3);
+}
+/*myFunction('abcdefg')
+Expected
+'abcd'
+myFunction('1234')
+Expected
+'1'
+myFunction('fgedcba')
+Expected
+'fged'*/
