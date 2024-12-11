e6cacb2053f75eb7c9a7c341280870df03643f58
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-12-11 16:46:55 +0200

    returning the last 3 letters

--- practice.js
+++ practice.js
@@ -88,3 +88,19 @@ Expected
 myFunction('fgedcba')
 Expected
 'dcba'*/
+// 6.Write a function that takes a string as argument. Extract the last 3 characters from the string. Return the result
+function myFunction(str) {
+  return str.slice(-3);
+}
+/*Test Cases:
+Attempted: 260165
+Solved: 90349
+myFunction('abcdefg')
+Expected
+'efg'
+myFunction('1234')
+Expected
+'234'
+myFunction('fgedcba')
+Expected
+'cba'*/
