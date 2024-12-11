664ec98d497f260adfc3334ece98d623ec4faf55
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-12-11 16:36:34 +0200

    type of variable

--- practice.js
+++ practice.js
@@ -31,4 +31,29 @@ function myFunction(a, b) {
   Expected
   false 
   */
- 
+  // 3.Write a function that takes a value as argument. Return the type of the value.
+  function myFunction(a) {
+    return typeof a;
+  }
+  /*Test Cases:
+  Attempted: 276535
+  Solved: 125649
+  myFunction(1)
+  Expected
+  'number'
+  myFunction(false)
+  Expected
+  'boolean'
+  myFunction({})
+  Expected
+  'object'
+  myFunction(null)
+  Expected
+  'object'
+  myFunction('string')
+  Expected
+  'string'
+  myFunction(['array'])
+  Expected
+  'object'*/
+  
