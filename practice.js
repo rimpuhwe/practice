0b57ee98613f1c69445d796f26bebe005b270114
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-12-11 16:35:41 +0200

    sum of 2 variables

---
+++ practice.js
@@ -0,0 +1,16 @@
+// 1.Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
+function myFunction(a, b) {
+  return a + b;
+}
+/* Test Cases:
+Attempted: 288838
+Solved: 163228
+myFunction(1,2)
+Expected
+3
+myFunction(1,10)
+Expected
+11
+myFunction(99,1)
+Expected
+100*/
