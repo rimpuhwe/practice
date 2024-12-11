b199c70314bafa2b5cb08f88488e3d46cbb44561
Raissa Micheline IMPUHWE MANZI <rimpuhwemanzi@gmail.com>
2024-12-11 16:36:12 +0200

    compare two variables

--- practice.js
+++ practice.js
@@ -14,3 +14,21 @@ Expected
 myFunction(99,1)
 Expected
 100*/
+// 2.Write a function that takes two values, say a and b, as arguments. Return true if the two values are equal and of the same type
+function myFunction(a, b) {
+    return a === b;
+  }
+  /*Test Cases:
+  Attempted: 387316
+  Solved: 135992
+  myFunction(2, 3)
+  Expected
+  false 
+  myFunction(3, 3)
+  Expected
+  true 
+  myFunction(1, '1')
+  Expected
+  false 
+  */
+ 
